6cc66da3157eb61149f0237be7204bafa421d843
Emmanuel Akpabio <emmanuelakpabio2@yahoo.com>
2023-02-23 08:49:49 -0800

    Add 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+/**
+ * getFibonacci - generates the nth fibonacci number
+ * @n: the nth fibonacci number
+ *
+ * Return: the nth fibonacci number
+ */
+int getFibonacci(int n)
+{
+	int num1;
+	int num2;
+	int sum;
+	int i;
+	int ret;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (i != n)
+		{
+			sum = num1 + num2;
+			num1 = num2;
+			num2 = sum;
+		}
+		else
+			ret = num2;
+	}
+	return (ret);
+}
+
+/**
+ * firstDigit - returns the first digit of a positive integer
+ * @n: The positive integer
+ *
+ * Return: first digit
+ */
+int firstDigit(int n)
+{
+	int first = 0;
+
+	while (n != 0)
+	{
+		first = n % 10;
+		n /= 10;
+	}
+	return (first);
+}
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int loop;
+	int fib;
+	int count;
+	int endCount;
+
+	loop = 1;
+	count = 0;
+	endCount = 4;
+	while (loop)
+	{
+		fib = getFibonacci(loop);
+		if (firstDigit(fib) == 1 || firstDigit(fib) == 2)
+		{
+			if (count < endCount)
+			{
+				printf("%d, ", fib);
+				count++;
+			}
+			else
+			{
+				printf("%d\n", fib);
+				loop = 0;
+			}
+		}
+	}
+	return (0);
+}
+
