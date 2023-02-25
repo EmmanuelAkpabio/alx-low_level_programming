eb5ddec9f981d9ba3e0b5cf8b26670038bfa6ff2
Emmanuel Akpabio <emmanuelakpabio2@yahoo.com>
2023-02-24 16:11:00 -0800

    correct code to include long long

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 
 /**
- * printBig - prints a long long to long
- * @n: long long integer
- *
- * Return: nothing
- */
-void printBig(unsigned long int n)
-{
-	printf("%lu", n / 10000000000);
-	printf("%lu", n % 10000000000);
-}
-
-/**
  * getFibonacci - generates fibonacci numbers up to n
  * @n: total count of numbers
  *
@@ -20,38 +8,42 @@ void printBig(unsigned long int n)
  */
 void getFibonacci(int n)
 {
-	unsigned long int num1;
-	unsigned long int num2;
-	unsigned long int sum;
+	unsigned long int num1, num2, sum, over_flow, value;
+	unsigned long int n_head, n_tail, m_head, m_tail, sum_head, sum_tail;
 	int i;
 
 	num1 = 1;
 	num2 = 2;
-
-	for (i = 1; i <= n; i++)
+	value = 10000000000;
+	for (i = 0; i <= n; i++)
 	{
-		if (i == n)
+		if (i == 90)
 		{
-			if (n > 92)
-				printBig(num1);
+			n_head = num1 / value;
+			n_tail = num1 % value;
+			m_head = num2 / value;
+			m_tail = num2 % value;
+		}
+		if (i > 90)
+		{
+			over_flow = (n_tail + m_tail) / value;
+			if (i == 98)
+				printf("%lu%lu\n", n_head, n_tail);
+			else
+				printf("%lu%lu, ", n_head, n_tail);
+			sum_head = (n_head + m_head) + over_flow;
+			sum_tail = (n_tail + m_tail) - (over_flow * value);
+			n_head = m_head;
+			n_tail = m_tail;
+			m_head = sum_head;
+			m_tail = sum_tail;
 		}
 		else
 		{
-			if (n > 92)
-			{
-				printBig(num1);
-				printf(", ");
-				sum = num1 + num2;
-				num1 = num2;
-				num2 = sum;
-			}
-			else
-			{
-				printf("%lu, ", num1);
-				sum = num1 + num2;
-				num1 = num2;
-				num2 = sum;
-			}
+			printf("%lu, ", num1);
+			sum = num1 + num2;
+			num1 = num2;
+			num2 = sum;
 		}
 	}
 }
