e73192bac7f0c48404a66e567c59b3004b37d7fb
Emmanuel Akpabio <emmanuelakpabio2@yahoo.com>
2023-02-25 09:30:47 -0800

    Modify if condition to validate 1st March of a leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
-		if (month > 2 && day > 60)
+		if (month > 2 && day >= 60) /* 03/20/2000 or 03/01/2000 */
 		{
 			day++;
 		}
