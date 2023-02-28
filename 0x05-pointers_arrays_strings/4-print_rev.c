216f40f2c4fbdac05e760b3b1d124b44d9be6efc
Emmanuel Akpabio <emmanuelakpabio2@yahoo.com>
2023-02-28 04:35:43 -0800

    add 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * print_rev - prints a string, in reverse,
+ * followed by a new line
+ * @s: string to be printed
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = _strlen(s);
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
