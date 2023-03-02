e366c3db8dcc1a51b4b69e7d7217fbf4d87087de
Emmanuel Akpabio <emmanuelakpabio2@yahoo.com>
2023-03-02 07:45:04 -0800

    Add 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: the string
+ *
+ * Returns: encoded string
+ */
+
+char *rot13(char *s)
+{
+	char arr[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char replace[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; arr[j] != '\0'; j++)
+		{
+			if (s[i] == arr[j])
+			{
+				s[i] = replace[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
