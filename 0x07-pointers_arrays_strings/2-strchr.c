31a59c757fc6b31f29e8e38172b66108a5696f33
Emmanuel Akpabio <emmanuelakpabio2@yahoo.com>
2023-03-07 04:36:41 -0800

    Add 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: the character
+ *
+ * Return: a pointer to the first occurence of the character c
+ * or NULL if the c is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	return (NULL);
+}
