7b7ce0075977de2851c1c2225822751a718ab7d1
Emmanuel Akpabio <emmanuelakpabio2@yahoo.com>
2023-03-16 17:37:29 -0700

    Include case where old and new sizes are negative

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,6 +15,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *new_ptr;
 	unsigned int i;
 
+	if (new_size < 0 || old_size < 0)
+		return (NULL);
+
 	if (new_size == old_size)
 		return (ptr);
 
