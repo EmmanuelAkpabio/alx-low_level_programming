40f717dbaf27cdf175b8c637978a419b64f6c09f
Emmanuel Akpabio <emmanuelakpabio2@yahoo.com>
2023-03-22 07:47:54 -0700

    consider when cmp is NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-
 /**
  * int_index - function that searches for an integer.
  * @array: the array
@@ -15,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
