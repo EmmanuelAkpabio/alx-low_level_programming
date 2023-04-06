75b3f0f8e3996ccfc2a4fabc616070359c6cc7bf
Emmanuel Akpabio <emmanuelakpabio2@yahoo.com>
2023-04-05 19:45:45 -0700

    Correct return pointer to new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - Inserts a new node at a gien position
+ * insert_nodeint_at_index - Inserts a new node at a given position
  * @head: double pointer to the head node
  * @idx: index of the list where the new node should be added.
  * @n: the integer to add
@@ -33,5 +33,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->next = temp->next;
 	temp->next = new;
 
-	return (*head);
+	return (new);
 }
