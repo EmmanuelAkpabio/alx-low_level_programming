9e1a12b68011bfaf1bb9771968a1ef68e87bd1a4
Emmanuel Akpabio <emmanuelakpabio2@yahoo.com>
2023-04-07 19:40:57 -0700

    refactor code

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,42 +9,19 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int mask = 1, len = 0, num = n;
-	int value = -1, num1 = n, idx = index;
-
-	/* handle negative num */
-	if (num1 < 0)
-		return (value);
-
-	/* handle negative index */
-	if (idx < 0)
-		return (value);
-
-	/* handle when num is 0 */
-	if (num == 0 && index == 0)
-		return (0);
-
-	/* get bit-lenght of num */
-	while (num > 0)
-	{
-		num >>= 1;
-		len++;
-	}
+	unsigned int len, mask = 1;
+	int value;
 
 	/* check if index is valid */
-	if (index < len)
-	{
-		/* update mask based on index */
-		mask <<= index;
+	len = sizeof(unsigned long int) * 8;
+	if (index > len)
+		return (-1)
 
-		/* get the value at index */
-		value = (n & mask) ? 1 : 0;
-	}
-	else
-	{
-		/* index is out of range */
-		value = -1;
-	}
+	/* update mask based on index */
+	mask <<= index;
+
+	/* get the value at index */
+	value = (n & mask) ? 1 : 0;
 
 	return (value);
 }
